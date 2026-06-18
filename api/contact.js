5de4d6bdd28df4a59a44782e3393cc659782b983
Leandro Peralta <lperalta13@gmail.com>
2026-06-18 14:44:49 -0400

    fix(contact): atualizar destinatário de email para refletir nova configuração

--- api/contact.js
+++ api/contact.js
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer'
 
 // const DESTINATARIOS = ['semproblema@gmail.com', 'ajuda@dothcom.net', 'dothcom@gmail.com', 'lperalta13@gmail.com']
-const DESTINATARIOS = ['lperalta13@gmail.com']
+const DESTINATARIOS = ['dothcom@gmail.com']
 const ACCENT_COLOR = '#2B00C9'
 const BRAND_LIGHT_COLOR = '#394AF7'
 const INK_COLOR = '#040407'
